20a4e69f2db721dc7f6b6982d0bc4bf86387afc2
Duong8888 <88546189+Duong8888@users.noreply.github.com>
2022-05-12 21:13:02 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-function open(){
-    // document.getElementsByTagName('ul').style.display = 'flex';
+var count = 0;
+function hi(){
+    if(count%2 == 0){
+        document.getElementById('menu-1').style.transform = 'scale(1)';
+        document.getElementById('menu-1').style.zIndex = '10';
+    }else{
+        document.getElementById('menu-1').style.transform = 'scale(0.001)';
+        document.getElementById('menu-1').style.zIndex = '-1';
+        
+    }
+    count++;
 }
